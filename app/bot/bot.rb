b745cec60b632b626bb736db5ce97b9d37df375a
Moufid <moufking337@gmail.com>
2019-04-29 16:08:42 +0100

    ok

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,5 +1,9 @@
+require 'facebook/messenger'
 include Facebook::Messenger
-
+Facebook::Messenger::Subscriptions.subscribe(
+    access_token: ENV["ACCESS_TOKEN"],
+    subscribed_fields: %w[feed mention name]
+  )
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
